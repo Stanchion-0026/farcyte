db53c9069d5407c008b9d5d8603bae206214b536
Iniquit <iniquitwrites@gmail.com>
2021-09-26 13:51:39 -0400

    WIP

--- functions/findPage.js
+++ functions/findPage.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const lunr = require('lunr');
+const si = require('search-index');
 
 module.exports = find;
 
@@ -8,36 +8,13 @@ module.exports = find;
 		.then(text => search(args, text));
 }
 
-function search(args, text) {
+async function search(args, text) {
 
 	const pageRegex = /(?<name>[\d]+\.+[\d]+)(?<text>[\s\S]+?)(?=[\d]+\.+[\d]+|$(?![\r\n])|\nCHAPTER)/gim;
 	const transcriptArray = [...text.matchAll(pageRegex)].map (e => Object.assign({}, e.groups));
 
-	const index = lunr(function() {
-		this.ref('name');
-		this.field('text');
-		this.field('name');
-		this.pipeline.remove(lunr.stemmer);
-		this.searchPipeline.remove(lunr.stemmer);
-		this.pipeline.remove(lunr.stopWordFilter);
-		this.searchPipeline.remove(lunr.stopWordFilter);
-		// this.pipeline.remove(lunr.trimmer);
-		// this.searchPipeline.add(lunr.trimmer);
-
-		// this.searchPipeline.add(lunr.trimmer);
-		// this.k1(1.3);
-		// this.b(0);
-
-		transcriptArray.forEach(doc => this.add(doc));
-	});
-	try {
-		const foundPage = index.search(args)[0].ref;
-
-		return { 'pageNumber': foundPage };
-	}
-
-	catch {
-		return { 'pageNumber': 'Couldn\'t find a match' };
-	}
+	const db = await si();
+	await db.PUT(transcriptArray);
+	return await db.QUERY({ SEARCH: [args] });
 
 }
